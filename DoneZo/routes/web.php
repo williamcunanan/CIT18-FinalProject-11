eab942a4e1b305559f812181d7174282f8179152
jessyforg <153629899+jessyforg@users.noreply.github.com>
2025-03-19 03:28:06 +0000

    Updated Routes to Task and Pomodoro UI

--- DoneZo/routes/web.php
+++ DoneZo/routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\PomodoroController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Authentication Routes
+Auth::routes();
+
+// Dashboard (User must be logged in)
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', [TaskController::class, 'index'])->name('dashboard');
+
+    // Task CRUD Routes
+    Route::resource('tasks', TaskController::class);
+
+    // Pomodoro Timer
+    Route::get('/pomodoro', [PomodoroController::class, 'index'])->name('pomodoro.index');
 });
