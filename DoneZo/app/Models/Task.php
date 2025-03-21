5822b7494dc797c40968bba188ac9c0039c593cd
neriquinto <145773221+neriquinto@users.noreply.github.com>
2025-03-21 04:15:38 +0000

    Task model updated

--- DoneZo/app/Models/Task.php
+++ DoneZo/app/Models/Task.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Task extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'status',
+        'user_id'
+    ];
+
+    public function user()
+    {
+        return $this->beongsTo(User::class);
+    }
 }
